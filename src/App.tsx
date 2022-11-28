6a16f757119d71bbd3353f8dc2bac14a8e1906f0
phobdaeneur <phobdawneur@gmail.com>
2022-11-28 16:51:51 +0700

    font-end phobdaw

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/phobdaw" element={<Phobdaw />} />
+        <Route path="/" element={<Phobdaw />} />
       </Routes>
     </Router>
   );
