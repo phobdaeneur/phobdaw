59c37303244c6f70d2e7a5e00039fe71e4b8b395
phobdaeneur <phobdawneur@gmail.com>
2022-11-29 08:51:48 +0700

    fontend phobdaw

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 // import "./script";
